e0823ccfc43b6362f416342c5012c7c063c38364
liviacavalcanti <livia.juliao@ccc.ufcg.edu.br>
2019-10-16 01:09:42 -0300

    chat problem

---
+++ chat.py
@@ -0,0 +1,17 @@
+word_input =  raw_input()
+word_match = "hello"
+match_index = 0
+letter =0
+while letter < len(word_input) and match_index < len(word_match):
+    if word_match[match_index] == word_input[letter]:
+        match_index += 1
+    letter += 1
+
+retorno = ""
+if match_index == len(word_match):
+    retorno = "YES"
+else:
+    retorno = "NO"
+
+print (retorno)
+    
