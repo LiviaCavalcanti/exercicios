55f3c53dac0195f242cb6cf851dd72b692a8bb90
liviacavalcanti <livia.juliao@ccc.ufcg.edu.br>
2019-10-16 00:43:29 -0300

    meat problem

---
+++ duffNmeat.py
@@ -0,0 +1,24 @@
+n_days = input()
+meat_amount = []
+meat_price = []
+total_meat = 0
+
+
+for i in range(n_days):
+    tuple_input = raw_input().split(' ')
+    meat_amount.append(int(tuple_input[0]))
+    total_meat += int(tuple_input[0])
+    meat_price.append(int(tuple_input[1]))
+
+best_value = total_meat * meat_price[0]
+total_meat -= meat_amount[0]
+partial_sum = meat_amount[0] * meat_price[0]
+
+for i in range(1, n_days):
+    partial_sum += meat_amount[i] * meat_price[i]
+    total_meat -= meat_amount[i]
+    temp = partial_sum + meat_price[i] * total_meat
+    if temp < best_value:
+        best_value = temp
+
+print best_value
